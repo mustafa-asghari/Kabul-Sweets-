1944574df06f7ee96b7491fda9038a8bbabfcf22
Mustafa Asghari <asghari.mustafa99@gmail.com>
2026-02-15 19:51:34 +1000

    improving frontend home page cake collection making to be zoomed out

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -131,7 +131,7 @@ export default function Home() {
           </ScrollReveal>
         </section>
 
-        <ActionBanner />
+        <ActionBanner zoomOut />
 
         <TestimonialSection />
       </main>
